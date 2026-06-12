5fc6e83383c8960003c61b3b4117c670df8ec23a
James Barrera <ja963mes@gmail.com>
2026-06-12 15:33:37 -0400

    edited YIN algorithm to fix missing low E and A detection

--- dsp_processor.py
+++ dsp_processor.py
@@ -9,13 +9,17 @@ def configure(rate):
 
 def preprocess_buffer(data):
     filtered_data = signal.filtfilt(b, a, data)
-    window_data = filtered_data * np.hanning(len(filtered_data))
-    return window_data
+    return filtered_data
 
-def  calculate_difference_function(data):
-    energy = np.sum(data ** 2)
-    result = 2 * energy - 2 * np.correlate(data, data, mode='full')[len(data)-1:]
-    return result
+def calculate_difference_function(data):
+    W = len(data) // 2
+    df = np.zeros(W)
+    
+    for tau in range(W):
+
+        df[tau] = np.sum((data[:W] - data[tau:tau+W]) ** 2)
+        
+    return df
 
 def calculate_cumulative_mean_normalized_difference_function(df):
     cmnd = df.copy()
@@ -25,19 +29,13 @@ def calculate_cumulative_mean_normalized_difference_function(df):
 
 def peak_valley_detection(cmnd):
     first_threshold = 0.15
-    index = None
-    for i in range(1, len(cmnd)-1):
-        if cmnd[i] < first_threshold:
-            index = i
-            break
-    if index is None:
-        return None
-
-    min_index = index
-    for i in range(index, len(cmnd)-1):
-        if cmnd[i] < cmnd[min_index]:
-            min_index = i
-    return min_index
+    for tau in range(1, len(cmnd) - 1):
+        if cmnd[tau] < first_threshold:
+            while tau + 1 < len(cmnd) and cmnd[tau + 1] < cmnd[tau]:
+                tau += 1
+            return tau
+            
+    return None
 
 def frequency_to_note_cent(freq):
     A4 = 440.0
